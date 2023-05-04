d20650fb67fabd4508a14aff5e0e234b00862b5b
Kyle Weiher <71463567+kyleweiher1@users.noreply.github.com>
2023-05-04 18:45:13 -0500

    Add files via upload
    This is the intital commit of the HikeHubv2 swiftUI app

---
+++ HikeHubV2/App/HikeHubV2App.swift
@@ -0,0 +1,17 @@
+//
+//  HikeHubV2App.swift
+//  HikeHubV2
+//
+//  Created by Kyle Weiher on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeHubV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
